df3f954b9355e0b5eb965e5abe33378b7601df95
김소현 <prrsh22@gmail.com>
2019-07-17 22:02:24 +0900

    새로운 dojo 앱을 생성하고, 등록

--- askdjango/askdjango/urls.py
+++ askdjango/askdjango/urls.py
@@ -19,4 +19,5 @@ from django.urls import include,path,re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^blog/',include('blog.urls')),
+    path('dojo/', include('dojo.urls'))
 ]
