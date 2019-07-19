aa1343ff1cbb31a3df669b8dc1dcae5be47871ad
김소현 <prrsh22@gmail.com>
2019-07-19 22:37:22 +0900

    django-debug-toolbar 셋업

--- askdjango/askdjango/settings.py
+++ askdjango/askdjango/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'debug_toolbar',
     'blog', 'dojo',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INTERNAL_IPS=['127.0.0.1']
--- askdjango/askdjango/urls.py
+++ askdjango/askdjango/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path,re_path
 
+from askdjango import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^blog/',include('blog.urls')),
     path('dojo/', include('dojo.urls'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns +=[
+        re_path(r'^__debug__/',include(debug_toolbar.urls)),
+    ]
