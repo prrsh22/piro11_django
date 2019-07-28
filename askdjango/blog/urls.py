5c44beb6135480108f91bc466ff55d1c09e1addb
김소현 <prrsh22@gmail.com>
2019-07-29 00:48:03 +0900

    blog.views.post_new/post_edit 뷰를 구현

--- askdjango/blog/urls.py
+++ askdjango/blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
     path('cbv/new/', views_cbv.post_new),
+    path('new/',views.post_new, name='post_new'),
+    path('<int:id>/edit/', views.post_edit, name='post_edit'),
 ]
