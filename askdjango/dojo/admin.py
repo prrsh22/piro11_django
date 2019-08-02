6abf33c3105efcd5a6d4e684adb09cbe62ce0433
김소현 <prrsh22@gmail.com>
2019-08-02 17:06:03 +0900

    dojo.models.GameUser 모델을 통해 unique_together 예시 추가

--- askdjango/dojo/admin.py
+++ askdjango/dojo/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from dojo.models import Post
+from dojo.models import Post, GameUser
 
 
 admin.site.register(Post)
+
+admin.site.register(GameUser)
